7717800fdb2ce411c597bcb3cbc8cdb4a8247d25
Teja Tummuru <tejasvinitummuru@tejas-mbp.lan>
2023-12-15 23:38:20 -0600

    band calculations and adding artists working with json

--- BattleOfTheBands/App.js
+++ BattleOfTheBands/App.js
@@ -1,63 +1,120 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Button, TextInput } from 'react-native';
-import * as AuthSession from 'expo-auth-session';
-import * as Random from 'expo-random';
-import ArtistSearchComponent from './frontend/ArtistSelection';
+// import React, { useState, useEffect } from 'react';
+// import { StyleSheet, View, Button, TextInput, TouchableOpacity, Text } from 'react-native';
+// import * as AuthSession from 'expo-auth-session';
+// import * as Random from 'expo-random';
+// import ArtistSearchComponent from './frontend/ArtistSelection';
+// import Band from './band'; // Assuming Band.js is in the same directory
 
-// You must replace 'yourapp' with your actual scheme from app.json
-const redirectUri = AuthSession.makeRedirectUri({
-  useProxy: true,
-  scheme: 'BattleOfTheBands',
-});
+// // You must replace 'yourapp' with your actual scheme from app.json
+// const redirectUri = AuthSession.makeRedirectUri({
+//   useProxy: true,
+//   scheme: 'BattleOfTheBands',
+// });
 
-const spotifyConfig = {
-  clientId: 'c935d06a336246adb4f76634e07b3079',
-  scopes: ['user-read-private', 'user-read-email', 'playlist-read-private', 'playlist-read-collaborative'],
-};
+// const spotifyConfig = {
+//   clientId: 'c935d06a336246adb4f76634e07b3079',
+//   scopes: ['user-read-private', 'user-read-email', 'playlist-read-private', 'playlist-read-collaborative'],
+// };
 
-export default function App() {
-  const [accessToken, setAccessToken] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
+// export default function App() {
+//   const [searchQuery, setSearchQuery] = useState('');
+//   const sampleArtists = ["Gracie Abrams", "Maisie Peters", "Travis Scott", "Taylor Swift", "Sarah Kinsley", "Ed Sheeran", "Conan Gray"];
+//   const [accessToken, setAccessToken] = useState('BQDElCm5DsYOD80X0HBKkViUPzo8WNsZyU47ZmwP9VGyR3fiji620kBdp3dAfgxnWAxhOZlteM1i4Or3yox0IVpwF-qK2fNxP9z7eL7shb6fs_niMYU');
+//   const [selectedArtistName, setSelectedArtistName] = useState('');
+//   const selectArtist = (artistName) => {
+//     setSelectedArtistName(artistName);
+//   };
+//   // Function to handle the authentication process
+//   const handleSpotifyLogin = async () => {
+//     // Generate a random string for the code verifier
+//     const codeVerifier = Random.getRandomBytes(64).toString('base64');
+//     const authUrl = `https://accounts.spotify.com/authorize?client_id=${spotifyConfig.clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(spotifyConfig.scopes.join(' '))}&response_type=code&code_challenge_method=S256&code_challenge=${codeVerifier}`;
+//     const response = await AuthSession.startAsync({ authUrl });
 
-  // Function to handle the authentication process
-  const handleSpotifyLogin = async () => {
-    // Generate a random string for the code verifier
-    const codeVerifier = Random.getRandomBytes(64).toString('base64');
-    const authUrl = `https://accounts.spotify.com/authorize?client_id=${spotifyConfig.clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(spotifyConfig.scopes.join(' '))}&response_type=code&code_challenge_method=S256&code_challenge=${codeVerifier}`;
-    const response = await AuthSession.startAsync({ authUrl });
+//     if (response.type === 'success') {
+//       const code = response.params.code;
+//       // Exchange the code for an access token
+//       // You'll need to do this step from your server, not directly from the app
+//     }
+//   };
 
-    if (response.type === 'success') {
-      const code = response.params.code;
-      // Exchange the code for an access token
-      // You'll need to do this step from your server, not directly from the app
-    }
+//   return (
+//     <View style={styles.container}>
+//       <TextInput
+//         style={styles.searchInput}
+//         value={searchQuery}
+//         onChangeText={setSearchQuery}
+//         placeholder="Search for Artists"
+//       />
+//       <Button title="Login with Spotify" onPress={handleSpotifyLogin} />
+//       <View style={styles.container}>
+//       {/* ... existing components ... */}
+//       <Band selectedArtistName={selectedArtistName} accessToken={accessToken} />
+//       <View>
+//         {sampleArtists.map(artistName => (
+//           <TouchableOpacity key={artistName} onPress={() => selectArtist(artistName)}>
+//             <Text>Add {artistName}</Text>
+//           </TouchableOpacity>
+//         ))}
+//       </View>
+//     </View>
+//       <ArtistSearchComponent accessToken={accessToken} searchQuery={searchQuery} />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   searchInput: {
+//     height: 40,
+//     width: '80%',
+//     borderColor: 'gray',
+//     borderWidth: 1,
+//     padding: 10,
+//   },
+// });
+
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, TouchableOpacity, Text } from 'react-native';
+import Band from './band'; // Make sure the path is correct
+
+const App = () => {
+  const [selectedArtistName, setSelectedArtistName] = useState('');
+  const sampleArtists = ["Artist 1", "Artist 2", "Artist 3", "Artist 4", "Artist 5", "Artist 6", "Artist 7"];
+
+  const selectArtist = (artistName) => {
+    setSelectedArtistName(artistName);
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    searchInput: {
+      height: 40,
+      width: '80%',
+      borderColor: 'gray',
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.searchInput}
-        value={searchQuery}
-        onChangeText={setSearchQuery}
-        placeholder="Search for Artists"
-      />
-      <Button title="Login with Spotify" onPress={handleSpotifyLogin} />
-      <ArtistSearchComponent accessToken={accessToken} searchQuery={searchQuery} />
+      {sampleArtists.map(artistName => (
+        <TouchableOpacity key={artistName} onPress={() => selectArtist(artistName)}>
+          <Text>Add {artistName}</Text>
+        </TouchableOpacity>
+      ))}
+      <Band selectedArtistName={selectedArtistName} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  searchInput: {
-    height: 40,
-    width: '80%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    padding: 10,
-  },
-});
+export default App;
--- BattleOfTheBands/band.js
+++ BattleOfTheBands/band.js
@@ -1,111 +1,254 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { View, Text, Alert } from 'react-native';
 
-const Band = () => {
-  const initialBand = {
-    pop: null,
-    rock: null,
-    hipHop: null,
-    instrumental: null,
-    rAndB: null,
-    indie: null,
-    country: null,
-  };
+// const Band = ({ accessToken }) => {
+//   const initialBand = {
+//     pop: null,
+//     rock: null,
+//     hipHop: null,
+//     instrumental: null,
+//     rAndB: null,
+//     indie: null,
+//     country: null,
+//   };
+
+//   const [band, setBand] = useState(initialBand);
+//   const [totalPopularity, setTotalPopularity] = useState(20);
+// //   const [accessToken, setAccessToken] = useState(''); // Set your access token here
+
+//   const handleSelectArtist = async (query) => {
+//     try {
+//       // Fetch artist data
+//       let response = await fetch('http://127.0.0.1:5000/search_artists', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ accessToken, query }),
+//       });
+//       const { artists } = await response.json();
+//       const artist = artists[0]; // Assume you want the first artist from the response
+
+//       // Check and update band
+//       const primaryGenre = artist.genres[0]; // Assuming the first genre is the primary
+//       const updatedBand = { ...band };
+
+//       if (!band[primaryGenre]) {
+//         updatedBand[primaryGenre] = artist;
+//       } else {
+//         // Try to fit the artist in another genre
+//         const availableGenre = Object.keys(band).find(genre => !band[genre]);
+//         if (availableGenre) {
+//           updatedBand[availableGenre] = artist;
+//         } else {
+//           alert('All genre slots are filled. Please remove an artist to add a new one.');
+//           return;
+//         }
+//       }
+
+//       setBand(updatedBand);
+//       updateBandPopularity(updatedBand, artist.id);
+//     } catch (error) {
+//       console.error('Error fetching artist:', error);
+//     }
+//   };
+
+//   const updateBandPopularity = async (updatedBand) => {
+//     let newPopularity = totalPopularity; // Base popularity
+  
+//     for (const genre in updatedBand) {
+//       const artist = updatedBand[genre];
+//       if (artist) {
+//         // Fetch artist's albums
+//         let response = await fetch('http://127.0.0.1:5000/artist_albums', {
+//           method: 'POST',
+//           headers: { 'Content-Type': 'application/json' },
+//           body: JSON.stringify({ accessToken, artistId: artist.id }),
+//         });
+//         const { albums } = await response.json();
+  
+//         let albumPopularityChange = 0;
+//         let relevantAlbumCount = 0;
+  
+//         for (const album of albums) {
+//           // Fetch album details
+//           response = await fetch('http://127.0.0.1:5000/album_details', {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({ accessToken, albumId: album.id }),
+//           });
+//           const { albumDetails } = await response.json();
+  
+//           // Check if album's genre matches the artist's current genre
+//           if (albumDetails.genres.includes(genre)) {
+//             relevantAlbumCount++;
+//             albumPopularityChange += calculatePopularityChange(artist.popularity, albumDetails.popularity);
+//           }
+//         }
+
+//       }
+//     }
+  
+//     setTotalPopularity(newPopularity);
+//   };
+  
+
+//   const calculatePopularityChange = (artistPopularity, albumPopularity) => {
+//     return albumPopularity - artistPopularity;
+//   };
+
+//   // Sample function to select an artist by name
+//   const addArtist = (artistName) => {
+//     handleSelectArtist(artistName);
+//   };
+
+//   return (
+//     <View>
+//       <Text>Total Band Popularity: {totalPopularity}</Text>
+//       {Object.entries(band).map(([genre, artist]) => (
+//         artist ? <Text key={genre}>{genre.toUpperCase()}: {artist.name}</Text> : null
+//       ))}
+//     </View>
+//   );
+// };
+
+// export default Band;
+
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import artistData from './artists_data.json'; // Path to your JSON file
+import albumData from './albums_data.json'; // Path to your JSON file
+
+const Band = ({ selectedArtistName }) => {
+    const initialBand = {
+        pop: [],
+        rock: [],
+        hipHop: [],
+        instrumental: [],
+        rAndB: [],
+        indie: [],
+        country: [],
+        world: [], // And so on for other genres...
+      };
+
+  const genreMapping = {
+    "pop": [
+        "pop", "cantopop", "indie-pop", "k-pop", "mandopop", "pop-film", "power-pop", "synth-pop", "j-pop", "j-idol"
+    ],
+    "rock": [
+        "alt-rock", "alternative", "british", "emo", "grunge", "hard-rock", "indie", "j-rock", "psych-rock", "punk", 
+        "punk-rock", "rock", "rock-n-roll", "rockabilly", "black-metal", "death-metal", "metal", "metal-misc", "metalcore",
+        "goth", "heavy-metal", "hardcore", "post-dubstep"
+    ],
+    "hipHop": [
+        "hip-hop", "club", "rap"
+    ],
+    "instrumental": [
+        "ambient", "classical", "instrumental", "jazz", "piano", "soundtracks", "bossanova", "children", "chill", 
+        "dance", "breakbeat", "chicago-house", "dancehall", "deep-house", "detroit-techno", "disney", "drum-and-bass",
+        "dub", "dubstep", "edm", "electro", "electronic", "house", "idm", "minimal-techno", "techno", "trance", 
+        "trip-hop", "swedish", "opera"
+    ],
+    "rAndB": [
+        "r-n-b", "soul", "disco", "blues", "funk", "groove", "motown", "gospel"
+    ],
+    "indie": [
+        "indie", "singer-songwriter", "songwriter", "acoustic", "folk", "garage", "lo-fi", "new-age"
+    ],
+    "country": [
+        "country", "americana", "bluegrass", "honky-tonk", "southern-rock", "cowpunk"
+    ],
+    "world": [
+        "afrobeat", "brazil", "latin", "reggae", "reggaeton", "salsa", "samba", "world-music", "african", 
+        "caribbean", "celtic", "flamenco", "indian", "latin-jazz", "middle-eastern", "tango", "anime", "comedy", 
+        "happy", "holidays", "kids", "malay", "movies", "mpb", "pagode", "party", "philippines-opm", "sertanejo",
+        "show-tunes", "ska", "summer"
+    ]
+    };
 
   const [band, setBand] = useState(initialBand);
-  const [totalPopularity, setTotalPopularity] = useState(20);
-  const [accessToken, setAccessToken] = useState(''); // Set your access token here
+  const [bandPopularity, setBandPopularity] = useState(20);
 
-  const handleSelectArtist = async (query) => {
-    try {
-      // Fetch artist data
-      let response = await fetch('http://127.0.0.1:5000/search_artists', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ accessToken, query }),
-      });
-      const { artists } = await response.json();
-      const artist = artists[0]; // Assume you want the first artist from the response
-
-      // Check and update band
-      const primaryGenre = artist.genres[0]; // Assuming the first genre is the primary
-      const updatedBand = { ...band };
-
-      if (!band[primaryGenre]) {
-        updatedBand[primaryGenre] = artist;
-      } else {
-        // Try to fit the artist in another genre
-        const availableGenre = Object.keys(band).find(genre => !band[genre]);
-        if (availableGenre) {
-          updatedBand[availableGenre] = artist;
-        } else {
-          alert('All genre slots are filled. Please remove an artist to add a new one.');
-          return;
+  const mapGenreToCategory = (genres) => {
+    for (const genre of genres) {
+      for (const category in genreMapping) {
+        if (genreMapping[category].includes(genre.toLowerCase())) { // Lowercase comparison
+          return category;
         }
       }
-
-      setBand(updatedBand);
-      updateBandPopularity(updatedBand, artist.id);
-    } catch (error) {
-      console.error('Error fetching artist:', error);
     }
+    return "world"; // Default category if no match
   };
 
-  const updateBandPopularity = async (updatedBand) => {
-    let newPopularity = totalPopularity; // Base popularity
-  
-    for (const genre in updatedBand) {
-      const artist = updatedBand[genre];
-      if (artist) {
-        // Fetch artist's albums
-        let response = await fetch('http://127.0.0.1:5000/artist_albums', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ accessToken, artistId: artist.id }),
-        });
-        const { albums } = await response.json();
-  
-        let albumPopularityChange = 0;
-        let relevantAlbumCount = 0;
-  
-        for (const album of albums) {
-          // Fetch album details
-          response = await fetch('http://127.0.0.1:5000/album_details', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ accessToken, albumId: album.id }),
-          });
-          const { albumDetails } = await response.json();
-  
-          // Check if album's genre matches the artist's current genre
-          if (albumDetails.genres.includes(genre)) {
-            relevantAlbumCount++;
-            albumPopularityChange += calculatePopularityChange(artist.popularity, albumDetails.popularity);
-          }
+  const handleSelectArtist = (artistName) => {
+    console.log('Current band state before update:', band);
+    const artistNameLower = artistName.toLowerCase();
+    const artist = artistData.artists.find(a => a.name.toLowerCase() === artistNameLower);
+
+    if (!artist) {
+        console.log('Artist not found');
+        return;
+    }
+
+    // Try to fit the artist into one of their genres
+    for (const genre of artist.genres) {
+        const mappedGenre = mapGenreToCategory([genre]);
+        if (!band[mappedGenre].length) {
+            console.log(`Adding ${artist.name} to ${mappedGenre}`);
+            setBand(prevBand => ({
+                ...prevBand,
+                [mappedGenre]: [...prevBand[mappedGenre], artist]
+            }));
+            updateBandPopularity(artist, mappedGenre);
+            return; // Exit after adding artist
         }
-
-      }
     }
+
+    console.log(`${artist.name} rocks in the genres ${artist.genres.join(', ')}, and we already have members in the band for those.`);
+};
+
+useEffect(() => {
+    console.log(`useEffect called with name: ${selectedArtistName}`);
+    if (selectedArtistName) {
+        handleSelectArtist(selectedArtistName);
+    }
+}, [selectedArtistName]);// Ensure that this effect runs only when selectedArtistName changes  
+
+const updateBandPopularity = (newArtist, genre) => {
+    console.log(`Updating band popularity for ${newArtist.name} in ${genre} genre.`);
+    let totalPopularityChange = 0;
+
+    const artistAlbums = albumData.albums.filter(album =>
+        album.artists.some(a => a.id === newArtist.id) && 
+        album.genres.some(albumGenre => mapGenreToCategory([albumGenre]) === genre)
+    );
+
+    console.log(`Found ${artistAlbums.length} albums for ${newArtist.name} in ${genre} genre.`);
+
+    artistAlbums.forEach(album => {
+        const albumPopularityChange = (album.popularity - newArtist.popularity) / newArtist.popularity;
+        console.log(`Album: ${album.name}, Album Popularity: ${album.popularity}, Artist Popularity: ${newArtist.popularity}, Change: ${albumPopularityChange}`);
+        totalPopularityChange += albumPopularityChange;
+    });
+
+    const newBandPopularity = bandPopularity + totalPopularityChange;
+    console.log(`Total Popularity Change: ${totalPopularityChange}, New Band Popularity: ${newBandPopularity.toFixed(2)}`);
+
+    setBandPopularity(prevPopularity => prevPopularity + totalPopularityChange);
+};
+
+
   
-    setTotalPopularity(newPopularity);
-  };
-  
-
-  const calculatePopularityChange = (artistPopularity, albumPopularity) => {
-    return albumPopularity - artistPopularity;
-  };
-
-  // Sample function to select an artist by name
-  const addArtist = (artistName) => {
-    handleSelectArtist(artistName);
-  };
-
   return (
-    <div>
-      <p>Total Band Popularity: {totalPopularity}</p>
-      {Object.entries(band).map(([genre, artist]) => (
-        artist ? <p key={genre}>{genre.toUpperCase()}: {artist.name}</p> : null
+    <View>
+      <Text>Total Band Popularity: {bandPopularity.toFixed(2)}</Text>
+      {Object.entries(band).map(([genre, artists]) => (
+        <View key={genre}>
+          <Text>{`${genre.toUpperCase()}:`}</Text>
+          {artists.map((artist, index) => (
+            <Text key={index}>{artist.name}</Text>
+          ))}
+        </View>
       ))}
-    </div>
+    </View>
   );
 };
 
